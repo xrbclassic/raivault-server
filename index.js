c8ac5c9b10ebf3683ea8886b41b8d2667c08dd44
xrbclassic <>
2019-09-27 21:23:39 +0100

    Update server config
    Change RPC and Node URL and switch off Redis for now.

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const timestamps = require('./timestamps');
 
 /** Configuration **/
 const nanoNodeUrl = process.env.NANO_NODE_URL || `http://172.31.7.100:7076`; // Nano node RPC url
-const nanoWorkNodeUrl = process.env.NANO_WORK_NODE_URL || `http://74.82.30.7:7076`; // Nano work node RPC url
+const nanoNodeUrl = process.env.NANO_NODE_URL || `http://[::1]:7002`; // Nano node RPC url
+const nanoWorkNodeUrl = process.env.NANO_WORK_NODE_URL || `http://[::1]:7002`; // Nano work node RPC url
 const listeningPort = process.env.APP_PORT || 9950; // Port this app will listen on
 
 const useDPoW = !!process.env.USE_DPOW || false; // Is the Distributed Proof of Work system used? (Requires API Key)
 
-const useRedisCache = !!process.env.USE_REDIS || true; // Change this if you are not running a Redis server.  Will use in memory cache instead.
-const redisCacheUrl = process.env.REDIS_HOST || `172.31.25.214`; // Url to the redis server (If used)
-const redisCacheTime = 60 * 60 * 24; // Store work for 24 Hours
-const memoryCacheLength = 800; // How much work to store in memory (If used)
+const useRedisCache = !!process.env.USE_REDIS || false; // Change this if you are not running a Redis server.  Will use in memory cache instead.
+// const redisCacheUrl = process.env.REDIS_HOST || `172.31.25.214`; // Url to the redis server (If used)
+// const redisCacheTime = 60 * 60 * 24; // Store work for 24 Hours
+// const memoryCacheLength = 800; // How much work to store in memory (If used)
 
 const express = require('express');
 const request = require('request-promise-native');
